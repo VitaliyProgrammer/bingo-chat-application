46f1857eeb0ff0f21dc6a3bab5ea51a0fb7878af
Vitaliy <vitaliyufc@gmail.com>
2026-05-12 12:20:45 +0300

    done application.properties and created application-example.properties file

--- src/main/java/org/example/configuration/websocket/WebSocketConfiguration.java
+++ src/main/java/org/example/configuration/websocket/WebSocketConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -24,5 +25,13 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         registration.enableSimpleBroker("/topic", "/queue");
         registration.setApplicationDestinationPrefixes("/app");
         registration.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+
+        registration.setMessageSizeLimit(64 * 1024);
+
+        registration.setSendBufferSizeLimit(512 * 1024);
     }
 }
